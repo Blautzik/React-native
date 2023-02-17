8620e51d2998013747ab54b5e87926ec85d77183
Federico Blautzik <blautzik.f@gmail.com>
2023-02-16 21:31:14 -0300

    primera entrega
    .

--- App.js
+++ App.js
@@ -1,20 +1,29 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, Image } from "react-native";
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+    <View style={styles.grid}>
+      <Image
+        source={require("./assets/foto-perfil.jpeg")}
+        style={styles.foteli}
+      />
+      <Text>Hola Coder!</Text>
     </View>
   );
 }
 
 const styles = StyleSheet.create({
-  container: {
+  grid: {
+    flexDirection: "row",
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center",
+  },
+  foteli: {
+    width: 50,
+    height: 50,
+    borderRadius: 50,
+    marginRight: 10,
   },
 });
